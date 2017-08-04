8c28d9a2f9fba8acbdb63d7dadd9b680e62f8184
qiracle <853370353@qq.com>
2017-08-04 13:55:36 +0800

    first commit

---
+++ quicksort.py
@@ -0,0 +1,9 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[int(len(arr) / 2)]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+print(quicksort([3,6,8,10,1,2,1,10,9]))
